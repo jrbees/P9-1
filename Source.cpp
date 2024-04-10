f8379d6e40103478d5484b26705dd68a4efc2a81
Jonah Bees <j-bees@onu.edu>
2024-04-10 16:57:27 -0400

    Random integers printed

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,23 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+#define ARRAY_SIZE 10
 
 int main() {
+    int array[ARRAY_SIZE];
+    int i;
 
+    // Seed the random number generator
+    srand(time(NULL));
 
+    // Initialize array with 4-digit random integers
+    printf("The random integers: ");
+    for (i = 0; i < ARRAY_SIZE; i++) {
+        array[i] = rand() % 9000 + 1000; // Generate random number between 1000 and 9999
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
